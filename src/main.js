4db13285eba63bdfdac9ecdbd1eb4ecec9d052b5
fahadaleem <faleem396@gmail.com>
2024-09-20 22:13:50 +0500

    update delivery note page, integrate api to fetch delivery note

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import { createApp } from "vue";
 import { router } from "./router";
+import VueHtmlToPaper from "vue-html-to-paper";
 import "./style.css";
 
 // Components
 import App from "./App.vue";
 
-createApp(App).use(router).mount("#app");
+const options = {
+  name: "_blank",
+  specs: ["fullscreen=yes", "titlebar=yes", "scrollbars=yes"],
+  styles: [
+    "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css",
+    "https://unpkg.com/kidlat-css/css/kidlat.css",
+  ],
+};
+
+createApp(App).use(VueHtmlToPaper, options).use(router).mount("#app");
--- src/services/apiUtilsService.js
+++ src/services/apiUtilsService.js
@@ -16,9 +16,15 @@ export const apiUtilsService = (() => {
     return axios.post(url, payload).then((res) => res.data);
   }
 
+  function getDeliveryNoteDetails(id) {
+    const url = `${import.meta.env.VITE_BACKEND_URL}/api/delivery/${id}`;
+    return axios.get(url).then((res) => res.data);
+  }
+
   return {
     getCustomers,
     getProducts,
+    getDeliveryNoteDetails,
     createDeliveryNote,
   };
 })();
